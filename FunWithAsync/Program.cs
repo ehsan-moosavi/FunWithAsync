994a8898bda1d99e522c6c2963bdb4d5d9263f93
Ehsan Moosavi <eh.moosavi@outlook.com>
2022-12-28 12:00:24 +0330

    Add project files.

---
+++ FunWithAsync/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunWithAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           string answer =await DoWOrkAsync();
+            Console.WriteLine(answer);
+            Console.WriteLine();
+            Console.WriteLine("Completed!!");
+        }
+        static async Task<string> DoWOrkAsync()
+        {
+            return await Task.Run(() => {
+                Thread.Sleep(5000);
+                return "Work Is Done!";
+            });
+        }
+    }
+}
